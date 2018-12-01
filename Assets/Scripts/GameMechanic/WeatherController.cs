6560e17ed9fb02a0949bf4a29a20bb016303b21d
NewUserName <sanineugene@gmail.com>
2018-12-01 11:30:41 +0300

    no message

--- Assets/Scripts/GameMechanic/WeatherController.cs
+++ Assets/Scripts/GameMechanic/WeatherController.cs
@@ -7,11 +7,15 @@ namespace Assets.Scripts.GameMechanic
     {
         public AuraVolume AuraVolumeSphere;
         public AuraVolume AuraVolumeGlobal;
+        public AuraLight Light;
+        public Aura Camera;
 
         void Start()
         {
             AuraVolumeSphere.enabled = true;
             AuraVolumeGlobal.enabled = true;
+            Light.enabled = true;
+            Camera.enabled = true;
         }
     }
 }
