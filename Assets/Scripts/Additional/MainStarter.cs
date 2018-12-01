75852473ba15fe95b33116a3689e608a9ff2caa7
Fedor <sumkin6969@gmail.com>
2018-12-01 19:22:12 +0300

    Финальный билд

---
+++ Assets/Scripts/Additional/MainStarter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Additional
+{
+    public class MainStarter:MonoBehaviour
+    {
+        public Image TutorialImage;
+        public Button ContinueButton;
+        public Image LogoImage;
+
+        private bool _anyKeyPressed = false;
+        public void Method()
+        {
+            if (!_anyKeyPressed)
+            {
+                LogoImage.gameObject.SetActive(false);
+                TutorialImage.gameObject.SetActive(true);
+                ContinueButton.gameObject.SetActive(true);
+                _anyKeyPressed = true;
+            }
+        }
+
+        void Update()
+        {
+            if (Input.anyKey)
+            {
+                Method();
+            }
+        }
+    }
+}
